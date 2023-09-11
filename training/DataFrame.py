1d3e77312c061796d4f7346ad97eb7b434a65235
erpupis <59801297+erpupis@users.noreply.github.com>
2023-09-11 02:16:37 +0200

    hybrid resampling

--- training/DataFrame.py
+++ training/DataFrame.py
@@ -2,6 +2,9 @@ from sklearn.preprocessing import MinMaxScaler
 from daos.input_dao import InputDao
 from core.database import Database
 from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
+from imblearn.pipeline import Pipeline
 import numpy as np
 from sklearn.model_selection import train_test_split
 
@@ -9,9 +12,37 @@ def initialize_database():
     db = Database()
     db.initialize()
     return db
-def oversample_data(X_train, y_train):
-    smote = SMOTE(random_state=42)
-    X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
+
+
+def hybrid_resample(X, y, desired_ratio=0.5):
+    """
+    Performs hybrid resampling on the given data.
+
+    Parameters:
+    - X: Features
+    - y: Labels
+    - desired_ratio: The desired ratio of minority class after resampling.
+
+    Returns:
+    - X_resampled: Resampled features
+    - y_resampled: Resampled labels
+    """
+
+    # Calculate the current class distribution
+    class_counts = Counter(y)
+    majority_class = max(class_counts, key=class_counts.get)
+    minority_class = min(class_counts, key=class_counts.get)
+
+    # Oversample using SMOTE to approach the desired ratio
+    smote_ratio = (class_counts[majority_class] * desired_ratio) / (1.0 - desired_ratio)
+    smote = SMOTE(sampling_strategy={minority_class: int(smote_ratio)})
+    X_smote, y_smote = smote.fit_resample(X, y)
+
+    # Undersample the majority class to achieve the desired ratio
+    rus_ratio = {majority_class: int(smote_ratio / desired_ratio)}
+    rus = RandomUnderSampler(sampling_strategy=rus_ratio)
+    X_resampled, y_resampled = rus.fit_resample(X_smote, y_smote)
+
     return X_resampled, y_resampled
 
 def fetch_player_data(db, player_name):
@@ -69,7 +100,7 @@ def process_data(player_name):
     X_train, X_test, y_train, y_test = perform_train_test_split(X, y)
 
     # Add oversampling here
-    X_train, y_train = oversample_data(X_train, y_train)
+    X_train, y_train = hybrid_sample_data(X_train, y_train)
 
     X_train, X_test, y_train, y_test = cast_data_types(X_train, X_test, y_train, y_test)
     return X_train, X_test, y_train, y_test
